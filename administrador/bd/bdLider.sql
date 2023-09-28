61fe8fb61e326783a0fede4a350d8261075d549c
mateusferian <mateusferian10@gmail.com>
2023-09-28 20:12:29 -0300

    :recycle: refactor: adjusting field and database table name

--- administrador/bd/bdLider.sql
+++ administrador/bd/bdLider.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Tempo de geração: 28/09/2023 às 11:04
+-- Tempo de geração: 28/09/2023 às 20:11
 -- Versão do servidor: 8.0.34-0ubuntu0.22.04.1
 -- Versão do PHP: 8.1.2-1ubuntu2.14
 
@@ -27,44 +27,43 @@ USE `bdLider`;
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `tbl_administrador`
+-- Estrutura para tabela `adminTable`
 --
 
-CREATE TABLE `tbl_administrador` (
+CREATE TABLE `adminTable` (
   `id` int NOT NULL,
-  `nome` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `name` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `email` varchar(40) NOT NULL,
-  `senha` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `tipo` int NOT NULL,
-  `dastaCadastro` date DEFAULT NULL
+  `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `registrationDate` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Despejando dados para a tabela `tbl_administrador`
+-- Despejando dados para a tabela `adminTable`
 --
 
-INSERT INTO `tbl_administrador` (`id`, `nome`, `email`, `senha`, `tipo`, `dastaCadastro`) VALUES
-(11, 'crip', 'crip', '$2y$10$JoHyw5u0Q5OyfBpIY5jt.ecy0gYXiuMmVu.84.T7vUyJ9MCm/ZykC', 2, NULL),
-(12, 'data', 'data', '$2y$10$F2VcHIAUZ7ZFaQv5DiSynu/XdoszqldqSYPBnEkQdtW3vZOSmW4IS', 2, '2023-09-03'),
-(13, 'as', 'as', '$2y$10$58hx7O9z9RV2pB6Yy6F8nOagak0xVUnnP.kFD5oSPcfC8qvTvWeeq', 2, '2023-09-03'),
-(14, 'sad', 'as', '$2y$10$7R1292uM1BdU/Mba0QtA6O4CdRN6lbpLLGbMCVPf6IFE7BUfVkGv6', 2, '2023-09-03'),
-(15, 'asd', 'sd', '$2y$10$lfilVSBo0LX6CcQHZ9r7Z.LqRM7TwUlE87.SfLz879zXtUtJ/PDTe', 2, '2023-09-03'),
-(16, 'asd', 'asd', '$2y$10$hGJAdDCnIkD1E31LbTA/Hej.h/fhvVKv31pWojTjyx/ky/hkpQ0eq', 2, '2023-09-03'),
-(17, 'asd', 'asd', '$2y$10$4iqeSE6bCDueSgD08mN1CepcM/4gYb/5MMCi9Ip482ReYGKG9R8qm', 2, '2023-09-03'),
-(18, 'zxcz', 'zxcxc', '$2y$10$EXeEcmpT9iEvqBjvyM7a6e61f80PPxIMb868ybBOGlDO7Dvva09sC', 2, '2023-09-03'),
-(19, 'qwe', 'asdad', '$2y$10$3mpDohUxvlytXtD6AprWN.f01TeJfaioiaIo2ATykB8gFAzB04gPy', 2, '2023-09-03'),
-(20, 'qwe', 'asdad', '$2y$10$RlAD7qaLAxLTwhDDtohbruOx5ZYDOGlq9pUAwHz3Gye93/AsW4Rgu', 2, '2023-09-03'),
-(21, 'mateus', 'm@gmail.com', '$2y$10$DtqzT6Ikjy2Abhl5dmb/gOoK8ZsxM/FHzOSpSSx8bC6uVVGrLRI1S', 2, '2023-09-13'),
-(22, 'sophia', 's@gmail.com', '$2y$10$McOBL4kHrTHpqIlEF0lW..KxjA67GBfBE.pGCUNC/8qAftnS1s9qu', 2, '2023-09-17');
+INSERT INTO `adminTable` (`id`, `name`, `email`, `password`, `registrationDate`) VALUES
+(11, 'crip', 'crip', '$2y$10$JoHyw5u0Q5OyfBpIY5jt.ecy0gYXiuMmVu.84.T7vUyJ9MCm/ZykC', NULL),
+(12, 'data', 'data', '$2y$10$F2VcHIAUZ7ZFaQv5DiSynu/XdoszqldqSYPBnEkQdtW3vZOSmW4IS', '2023-09-03'),
+(13, 'as', 'as', '$2y$10$58hx7O9z9RV2pB6Yy6F8nOagak0xVUnnP.kFD5oSPcfC8qvTvWeeq', '2023-09-03'),
+(14, 'sad', 'as', '$2y$10$7R1292uM1BdU/Mba0QtA6O4CdRN6lbpLLGbMCVPf6IFE7BUfVkGv6', '2023-09-03'),
+(15, 'asd', 'sd', '$2y$10$lfilVSBo0LX6CcQHZ9r7Z.LqRM7TwUlE87.SfLz879zXtUtJ/PDTe', '2023-09-03'),
+(16, 'asd', 'asd', '$2y$10$hGJAdDCnIkD1E31LbTA/Hej.h/fhvVKv31pWojTjyx/ky/hkpQ0eq', '2023-09-03'),
+(17, 'asd', 'asd', '$2y$10$4iqeSE6bCDueSgD08mN1CepcM/4gYb/5MMCi9Ip482ReYGKG9R8qm', '2023-09-03'),
+(18, 'zxcz', 'zxcxc', '$2y$10$EXeEcmpT9iEvqBjvyM7a6e61f80PPxIMb868ybBOGlDO7Dvva09sC', '2023-09-03'),
+(19, 'qwe', 'asdad', '$2y$10$3mpDohUxvlytXtD6AprWN.f01TeJfaioiaIo2ATykB8gFAzB04gPy', '2023-09-03'),
+(20, 'qwe', 'asdad', '$2y$10$RlAD7qaLAxLTwhDDtohbruOx5ZYDOGlq9pUAwHz3Gye93/AsW4Rgu', '2023-09-03'),
+(21, 'mateus', 'm@gmail.com', '$2y$10$DtqzT6Ikjy2Abhl5dmb/gOoK8ZsxM/FHzOSpSSx8bC6uVVGrLRI1S', '2023-09-13'),
+(22, 'sophia', 's@gmail.com', '$2y$10$McOBL4kHrTHpqIlEF0lW..KxjA67GBfBE.pGCUNC/8qAftnS1s9qu', '2023-09-17');
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices de tabela `tbl_administrador`
+-- Índices de tabela `adminTable`
 --
-ALTER TABLE `tbl_administrador`
+ALTER TABLE `adminTable`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -72,9 +71,9 @@ ALTER TABLE `tbl_administrador`
 --
 
 --
--- AUTO_INCREMENT de tabela `tbl_administrador`
+-- AUTO_INCREMENT de tabela `adminTable`
 --
-ALTER TABLE `tbl_administrador`
+ALTER TABLE `adminTable`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
