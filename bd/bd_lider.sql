9f2e87d93e8fd1b34d0a25517bcc91ffeb22213d
mateusferian <mateusferian10@gmail.com>
2023-12-14 15:44:21 -0300

    :recycle: refactor: refactoring dataBase

--- bd/bd_lider.sql
+++ bd/bd_lider.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Tempo de geração: 14/12/2023 às 15:41
+-- Tempo de geração: 14/12/2023 às 15:43
 -- Versão do servidor: 8.0.35-0ubuntu0.22.04.1
 -- Versão do PHP: 8.1.2-1ubuntu2.14
 
